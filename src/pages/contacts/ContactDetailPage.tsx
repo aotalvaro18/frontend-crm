284bf0e0156b173fd4157e7141d9c8b129952251
aotalvaro <otalvarog12@hotmail.com>
2025-08-20 14:22:18 -0500

    Guardar datos al actualizar y mostrar preferencias de comunicacion marketing

--- src/pages/contacts/ContactDetailPage.tsx
+++ src/pages/contacts/ContactDetailPage.tsx
@@ -6,7 +6,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 //import { ArrowLeft, Edit, Trash2 } from 'lucide-react';
 
 // ============================================
-// HOOKS DESACOPLADOS
+// HOOKS DESACOPLADOS OJO
 // ============================================
 
 import { 
